957dda171b375f6509a74a5622a119061e57dd8b
Sanjay Kushwah <sanjaykushwah0601@gmail.com>
2019-09-21 03:48:06 +0530

    Fix load type issue

--- app/src/main/java/com/freight/shipper/model/PastLoad.kt
+++ app/src/main/java/com/freight/shipper/model/PastLoad.kt
@@ -15,6 +15,7 @@ class PastLoad(val id: Long, val customerName: String) {
     val pickTime: String = "10:00 AM, 22 Jul"
     val destinationTime: String = "10:00 AM, 23 Jul"
     val amount: String = "120.00"
+    val loadType: String = "Test"
     val goodsName: String = "Electronics"
     val paidBy: String = "Receiver"
     val timeLeftToPickup: String = "00:57 Hrs"
