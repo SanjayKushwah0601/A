ae544605aeaf672bdcd7fa9c35ab43feb02dde15
Sanjay Kushwah <sanjaykushwah0601@gmail.com>
2019-10-19 22:58:30 +0530

    Invoice screen design

---
+++ app/src/main/java/com/freight/shipper/utils/customviews/CustomScrollView.kt
@@ -0,0 +1,39 @@
+package com.freight.shipper.utils.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.ScrollView
+
+/**
+ * @CreatedBy Sanjay Kushwah on 10/19/2019.
+ * sanjaykushwah0601@gmail.com
+ */
+class CustomScrollView : ScrollView {
+
+    var isEnableScrolling = true
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int)
+            : super(context, attrs, defStyle) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context) : super(context) {}
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        return if (isEnableScrolling) {
+            super.onInterceptTouchEvent(ev)
+        } else {
+            false
+        }
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        return if (isEnableScrolling) {
+            super.onTouchEvent(ev)
+        } else {
+            false
+        }
+    }
+}
